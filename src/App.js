de898b124083fb413b06ba4c59b580630034c834
Aaron Lee <darkrai1001@yahoo.com>
2020-09-06 20:43:37 -0700

    add socket connectio to App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import Login from './components/Login'
 import SignUp from './components/SignUp'
 import Main from './components/Main'
 import '../src/index.css'
+import socketIOClient from "socket.io-client";
+
+
+const ENDPOINT = "http://127.0.0.1:4001";
+// reference https://www.valentinog.com/blog/socket-react/
 
 class App extends Component {
   render() {
+    const [response, setResponse] = React.useState("");
+    useEffect(() => {
+      const socket = socketIOClient(ENDPOINT);
+      socket.on("FromAPI", data => {
+        setResponse(data);
+      });
+    }, []);
+
     return (
       <BrowserRouter>
         <div className="App">
